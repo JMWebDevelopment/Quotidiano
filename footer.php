d3ff7999d27dd88a409e22b8d4feaee8d3f83561
ViewFromTheBox <jacob.martella@att.net>
2016-12-14 19:42:27 -0600

    Styled footer

--- footer.php
+++ footer.php
@@ -9,21 +9,7 @@
 ?>
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<div class="large-12 medium-12 columns">
-								<nav role="navigation">
-		    						<?php wp_nav_menu(array(
-										    'container' => 'false',                              // Remove nav container
-										    'menu' => __( 'Footer Links', 'theme-slug' ),   	// Nav name
-										    'menu_class' => 'menu',      					// Adding custom nav class
-										    'theme_location' => 'footer-links',             // Where it's located in the theme
-										    'depth' => 0,                                   // Limit the depth of the nav
-										    'fallback_cb' => ''  							// Fallback function
-								    )); ?>
-		    					</nav>
-		    				</div>
-							<div class="large-12 medium-12 columns">
-								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-							</div>
+							<p class="source-org copyright">&copy; <?php _e( 'Copyright', 'quotidiano' ); ?> <?php echo date('Y'); ?> &bull; <a href="<?php echo esc_url( get_home_url( '/' ) ); ?>"><?php bloginfo('name'); ?> &bull; <a href="http://www.jacobmartella.com/wordpress/wordpress-theme/quotidiano-wordpress-theme">Quotidiano Theme</a></p>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
 				</div>  <!-- end .main-content -->
