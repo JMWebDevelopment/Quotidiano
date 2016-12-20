2f51b0d9d0defa8d675e2e2e843c0faec27fa47a
ViewFromTheBox <jacob.martella@att.net>
2016-12-19 21:53:48 -0600

    Styled responsive footer template

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 ?>
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<p class="source-org copyright">&copy; <?php _e( 'Copyright', 'quotidiano' ); ?> <?php echo date('Y'); ?> &bull; <a href="<?php echo esc_url( get_home_url( '/' ) ); ?>"><?php bloginfo('name'); ?> &bull; <a href="http://www.jacobmartella.com/wordpress/wordpress-theme/quotidiano-wordpress-theme">Quotidiano Theme</a></p>
+                            <p class="source-org copyright">&copy; <?php _e( 'Copyright', 'quotidiano' ); ?> <?php echo date('Y'); ?> <span class="hide-for-small-only">&bull;</span><span class="show-for-small-only"><br /></span> <a href="<?php echo esc_url( get_home_url( '/' ) ); ?>"><?php bloginfo('name'); ?></a> <span class="hide-for-small-only">&bull;</span><span class="show-for-small-only"><br /></span> <a href="http://www.jacobmartella.com/wordpress/wordpress-theme/quotidiano-wordpress-theme">Quotidiano Theme</a></p>
 						</div> <!-- end #inner-footer -->
 					</footer> <!-- end .footer -->
 				</div>  <!-- end .main-content -->
